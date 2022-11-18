3ab2b955bec8162682678884f2f3bb4e759b8edf
Ерохин Максим <110667386+Makas888@users.noreply.github.com>
2022-11-18 21:02:17 +0200

    Update settings.py

--- django_restaurant/settings.py
+++ django_restaurant/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure--95a3g^u+lq5^kj8d+-57zcmikb-fi$dn(oo+s@knnr1prokmc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
